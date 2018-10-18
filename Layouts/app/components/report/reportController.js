c49e0f0512262e3df469083d8f763c1b4aeaf9df
Wang, Elvis <ElWang@golder.com>
2018-10-18 17:08:33 -0600

    init

---
+++ Layouts/app/components/report/reportController.js
@@ -0,0 +1,45 @@
+﻿app.config(function ($mdIconProvider) {
+    $mdIconProvider
+        .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
+})
+app.controller('reportController', function ($scope) {
+    $scope.Message = "It's a report configure interface for IOL!";
+    var imagePath = 'img/list/60.jpeg';    
+    $scope.reports = [
+        {
+            type: imagePath,
+            title: 'Brunch this weekend?',
+            createdby: 'Min Li Chan',
+            created: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            type: imagePath,
+            title: 'Brunch this weekend?',
+            createdby: 'Min Li Chan',
+            created: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            type: imagePath,
+            title: 'Brunch this weekend?',
+            createdby: 'Min Li Chan',
+            created: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            type: imagePath,
+            title: 'Brunch this weekend?',
+            createdby: 'Min Li Chan',
+            created: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            type: imagePath,
+            title: 'Brunch this weekend?',
+            createdby: 'Min Li Chan',
+            created: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+    ];
+});
